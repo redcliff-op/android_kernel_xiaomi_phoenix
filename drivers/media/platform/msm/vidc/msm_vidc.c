ed8fce58a5ecf99a297d13377771915a34c8fdeb
Adam W. Willis <return.of.octobot@gmail.com>
2020-04-22 02:18:20 -0400

    msm: vidc: Silence video instance logging
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: redcliff-op <devyashjangid@gmail.com>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1903,7 +1903,7 @@ void *msm_vidc_open(int core_id, int session_type)
 		goto err_invalid_core;
 	}
 #ifdef CONFIG_DEBUG_KERNEL
-	pr_info(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
+	pr_debug(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
 		"info", inst, session_type);
 #endif
 	mutex_init(&inst->sync_lock);
@@ -2190,7 +2190,7 @@ int msm_vidc_destroy(struct msm_vidc_inst *inst)
 
 	msm_vidc_debugfs_deinit_inst(inst);
 #ifdef CONFIG_DEBUG_KERNEL
-	pr_info(VIDC_DBG_TAG "Closed video instance: %pK\n",
+	pr_debug(VIDC_DBG_TAG "Closed video instance: %pK\n",
 			"info", inst);
 #endif
 	kfree(inst);
