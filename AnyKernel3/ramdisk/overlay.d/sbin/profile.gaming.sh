b785bef89a9d605425a1f9e02f7e2c0c3638d40c
GtrCraft <nickvanbokhorst@hotmail.com>
2021-04-29 21:33:22 +0530

    Anykernel3: Add Spectrum Support
    Signed-off-by: redcliff-op <devyashjangid@gmail.com>

---
+++ AnyKernel3/ramdisk/overlay.d/sbin/profile.gaming.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+
+# Gaming
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 
+echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor 
+echo "1804800" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo "2208000" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+echo "1497600" > /sys/module/cpu_input_boost/parameters/input_boost_freq_little
+echo "1324800" > /sys/module/cpu_input_boost/parameters/input_boost_freq_big
+echo "128" > /sys/module/cpu_input_boost/parameters/input_boost_duration 
+echo "1804800" > /sys/module/cpu_input_boost/parameters/max_boost_freq_little
+echo "2208000" > /sys/devices/system/cpu/cpu6/cpufreq/max_boost_freq_big
+echo "performance" > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+
