c0f5992daf061ed315aa24195e76d142cec2c973
redcliff-op <devyashjangid@gmail.com>
2021-06-07 11:05:03 +0530

    build.sh: Adapt Build script for DTB Appended Builds

--- build.sh
+++ build.sh
@@ -1,14 +1,31 @@
+echo checking for repo update
+git config pull.rebase false
+git pull
+
+#clone or update clang if its alreay exists
+set -e
+
+if [ -r clang ]; then
+  echo clang found!
+  cd clang
+  git config pull.rebase false
+  cd ..
+  
+
+else
+  echo clang not found!, git cloning it now....
+  git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+
+fi
+
 KERNEL_DEFCONFIG=phoenix_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
 KERNELDIR=$PWD/
-FINAL_KERNEL_ZIP=Redcliff_2.7X.zip
+FINAL_KERNEL_ZIP=Redcliff_v2.8X.zip
 export PATH="${PWD}/clang/bin:${PATH}"
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_COMPILER_STRING="$(${PWD}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-IMAGE_GZ=$PWD/out/arch/arm64/boot/Image.gz
-DTB=$PWD/out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb
-DTBO_IMG=$PWD/out/arch/arm64/boot/dtbo.img
 # Speed up build process
 MAKE="./makeparallel"
 
@@ -19,56 +36,53 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-# Clean build always lol
+# Clean build 
 echo "**** Cleaning ****"
 mkdir -p out
 make O=out clean
 
-echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
-echo -e "$blue***********************************************"
 echo "          BUILDING KERNEL          "
-echo -e "***********************************************$nocol"
+
 make $KERNEL_DEFCONFIG O=out
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       NM=llvm-nm \
                       OBJCOPY=llvm-objcopy \
                       OBJDUMP=llvm-objdump \
                       STRIP=llvm-strip
 
-echo "**** Verify target files ****"
-if [ ! -f "$IMAGE_GZ" ]; then
-    echo "!!! Image.gz not found"
-    exit 1
-fi
-if [ ! -f "$DTB" ]; then
-    echo "!!! dtb not found"
-    exit 1
-fi
-if [ ! -f "$DTBO_IMG" ]; then
-    echo "!!! dtbo.img not found"
-    exit 1
-fi
+echo "**** Verify Image.gz-dtb & dtbo.img ****"
+ls $PWD/out/arch/arm64/boot/Image.gz-dtb
+ls $PWD/out/arch/arm64/boot/dtbo.img
 
-echo "**** Moving target files ****"
-mv $IMAGE_GZ $ANYKERNEL3_DIR/Image.gz
-mv $DTB $ANYKERNEL3_DIR/dtb
-mv $DTBO_IMG $ANYKERNEL3_DIR/dtbo.img
+# Anykernel 3 time!!
+echo "**** Verifying AnyKernel3 Directory ****"
+ls $ANYKERNEL3_DIR
+echo "**** Removing leftovers ****"
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+
+echo "**** Copying Image.gz-dtb & dtbo.img ****"
+cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
 
 echo "**** Time to zip up! ****"
 cd $ANYKERNEL3_DIR/
-zip -r9 $FINAL_KERNEL_ZIP *
+zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $KERNELDIR/$FINAL_KERNEL_ZIP
 
-echo "**** Removing leftovers ****"
+echo "**** Done, here is your sha1 ****"
 cd ..
-rm $ANYKERNEL3_DIR/Image.gz
-rm $ANYKERNEL3_DIR/dtb
-rm $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf out/
 
-mv -f $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP out/
-
-echo "Check out/$FINAL_KERNEL_ZIP"
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+sha1sum $KERNELDIR/$FINAL_KERNEL_ZIP
